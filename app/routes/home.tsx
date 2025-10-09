e0c7e48656e3eda5171915cf06c5a048644e0718
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-10-09 15:38:41 -0700

    Developing home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-no-repeat bg-cover">
+   <section className="main-section">
+    <div className="page-heading">
+      <h1 className="hero-title">Track Your Application and Resume ratings</h1>
+      <h2 className="hero-subtitle">Get real-time feedback on your resume and job applications with our AI-powered analyzer. Improve your chances of landing your dream job!</h2>
+      </div>
+   </section>
+  </main>;
 }
